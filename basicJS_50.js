c07c884869bb539dc3874b91f04d7227f495bf88
Miahri <mahrihommadova@gmail.com>
2022-12-06 21:45:49 +0300

    Added tasks with switch statements

--- basicJS_50.js
+++ basicJS_50.js
@@ -104,3 +104,50 @@ function testSize(num) {
 }
 testSize(7);
 
+//switch statements
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    return answer;
+}
+caseInSwitch(1);
+
+//Switch statement with multiple identical options
+function sequentialSizes(val) {
+    let answer = "";
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    }
+    return answer;
+}
+sequentialSizes(1);
+
+
