86ded36d4e48fe6b33e2bd6f26807c492a3e4e22
Miahri <mahrihommadova@gmail.com>
2022-12-06 21:32:36 +0300

    Added tasks with if else statements

--- basicJS_50.js
+++ basicJS_50.js
@@ -85,3 +85,22 @@ function testLogicalOr(val) {
     return "Inside";
 }
 testLogicalOr(15);
+
+//Chaining IF ELSE statements
+function testSize(num) {
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium"
+    } else if (num < 20) {
+        return "Large"
+    } else if (num >= 20) {
+        return "Huge";
+    } else {
+        return "Change Me";
+    }
+}
+testSize(7);
+
