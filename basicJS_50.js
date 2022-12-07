9f3dfe145e902c1cf5742e1dda090273139a5ca1
Miahri <mahrihommadova@gmail.com>
2022-12-07 22:48:05 +0300

    Added nested Objects

--- basicJS_50.js
+++ basicJS_50.js
@@ -194,3 +194,26 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+
+//Accessing nested objects
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[2];
