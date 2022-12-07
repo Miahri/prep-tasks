fe16be404531944ca95099ac4cd1b24c07750fb6
Miahri <mahrihommadova@gmail.com>
2022-12-07 22:19:36 +0300

    Added function with cards counting

--- basicJS_50.js
+++ basicJS_50.js
@@ -151,3 +151,27 @@ function sequentialSizes(val) {
 sequentialSizes(1);
 
 
+//Counting cards
+let count = 0;
+
+function cc(card) {
+    // Only change code below this line
+    if (card > 1 && card < 7) {
+        count++;
+    } else if (card > 6 && card < 10) {
+        count;
+    } else if (card === 10 || card == 'J' || card == 'Q' || card == 'K' || card == 'A'){
+        count--;
+    } else {
+        return "Change Me";
+    }
+
+    if (count > 0){
+        return count + ' Bet';
+    } else {
+        return count + ' Hold'
+    }
+    // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
