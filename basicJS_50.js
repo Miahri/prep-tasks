33991433831e2579972f8b85b648365127e79138
Miahri <mahrihommadova@gmail.com>
2022-12-08 21:00:00 +0300

    Added function with ternary operator

--- basicJS_50.js
+++ basicJS_50.js
@@ -324,3 +324,10 @@ function convertToInteger(str) {
 }
 
 convertToInteger("10011");
+
+//ternary operator
+function checkEqual(a, b) {
+    return a == b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
